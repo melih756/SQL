542ce9aaaa766ec74b58619f13a4d7db3a4deaad
melihtürkmen <74192618+melih756@users.noreply.github.com>
2022-08-01 14:41:56 +0300

    sql çalışmaları 2

---
+++ sql2.sql
@@ -0,0 +1,4 @@
+Select * from film where replacement_cost between 12.99 and 16.99 
+Select * from actor where first_name  In ('Penelope','Nick','Ed')
+
+Select * from film where rental_rate In(0.99,2.99,4,99) and replacement_cost In(12.99,15.99,28.99)
